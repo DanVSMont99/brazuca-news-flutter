3771fa90228918ac61d859e660fd8ce1a3b1ee5e
Daniel Vitor <danielvds348@gmail.com>
2020-06-22 11:10:09 -0300

    Home adicionada

---
+++ android/app/src/main/kotlin/com/dvs/brazucaNews/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.dvs.brazucaNews
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:brazucaNews/screens/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'News Brasuca',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        brightness: Brightness.dark
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
